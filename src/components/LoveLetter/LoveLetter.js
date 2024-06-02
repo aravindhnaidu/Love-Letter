e1318279e3d154a74384cff4916fe4f65c691bbe
BANDLA ARAVIND KUMAR <142375952+aravindhnaidu@users.noreply.github.com>
2024-06-02 21:51:21 +0530

    first commit

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,50 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './picchi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+      Bangaram🖤,<br />
+      Bangaram🖤endukeey naa life loki vachav aslee naaku em raaadhu nuvvu vachaka nuvvu tappa em ldhuuu inkaa roju matladaka pothe edho prapamcham agipoyyinatuu entoo 
+      last ga okati cepthaa ninnu matram appudu vadhulukonu love you forever the pichhi ga
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
+
+
+
